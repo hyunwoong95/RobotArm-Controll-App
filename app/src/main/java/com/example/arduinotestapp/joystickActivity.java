79fe577e6cb5d680ec12a457f2b25a3080125307
KHW\김현웅 <hyunwoongkim1234@gmail.com>
2024-08-21 17:45:09 +0900

    joyStick lib

--- app/src/main/java/com/example/arduinotestapp/joystickActivity.java
+++ app/src/main/java/com/example/arduinotestapp/joystickActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.joystick.JoystickView;
+
 public class joystickActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joystick);
+
+        JoystickView joystickView = findViewById(R.id.joystick);
+
     }
 
 }
